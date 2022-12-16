abd8f0a1a33b5d7576ec56e3a34debeef83c6347
Mansour Behabadi <mbehabadi@mathspace.com.au>
2022-12-16 19:58:06 +1100

    Bump setup script version

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Used to download and install lambdafy on a linux/amd64 host
 set -euo pipefail
-_ver="${1:-0.17}"
+_ver="${1:-0.18}"
 cd ~
 echo "Downloading lambdafy version $_ver ..."
 wget -qO- https://github.com/mathspace/lambdafy/releases/download/v${_ver}/lambdafy_${_ver}_linux_amd64.tar.gz |
