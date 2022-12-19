67167de1752f0d3c0fa397af10a77f076f6da450
Mansour Behabadi <mbehabadi@mathspace.com.au>
2022-12-19 18:30:02 +1100

    Use JSON for IAM policies to make it easier to copy elsewhere

--- example-role.tf
+++ example-role.tf
@@ -1,25 +1,33 @@
 resource "aws_iam_role" "fn" {
-  name = "my-custom-function" // TODO modify before use
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principals = [{
-        Type        = "Service"
-        Identifiers = ["lambda.amazonaws.com"]
-      }]
-      Actions = ["sts:AssumeRole"]
+  name               = "my-custom-function" // TODO modify before use
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [{
+      "Effect": "Allow",
+      "Principals": [{
+        "Type": "Service",
+        "Identifiers": ["lambda.amazonaws.com"]
+      }],
+      "Actions": ["sts:AssumeRole"]
     }]
-  })
+  }
+  EOF
   inline_policy {
     name = "main"
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        // Allow VPC access from lambda
+    // TODO add your custom statements after the main one below. E.g.:
+    // {
+    //   "Effect": "Allow",
+    //   "Action": ["ssm:GetParameter"],
+    //   "Resource": ["arn:aws:ssm:*:*:parameter/my_fn/*"]
+    // }
+    policy = <<-EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
         {
-          Effect = "Allow"
-          Action = [
+          "Effect": "Allow",
+          "Action": [
             "logs:CreateLogGroup",
             "logs:CreateLogStream",
             "logs:PutLogEvents",
@@ -27,17 +35,12 @@ resource "aws_iam_role" "fn" {
             "ec2:DescribeNetworkInterfaces",
             "ec2:DeleteNetworkInterface",
             "ec2:AssignPrivateIpAddresses",
-            "ec2:UnassignPrivateIpAddresses",
-          ]
-          Resource = ["*"]
-        },
-        // TODO your custom statements go here. E.g.:
-        // {
-        //   Effect   = "Allow"
-        //   Action   = ["ssm:GetParameter"]
-        //   Resource = ["arn:aws:ssm:*:*:parameter/my_fn/*"]
-        // },
+            "ec2:UnassignPrivateIpAddresses"
+          ],
+          "Resource": ["*"]
+        }
       ]
-    })
+    }
+    EOF
   }
 }
