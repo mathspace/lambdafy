9ccc6f2589f38acba4b0b9c8d5d437f87770b215
Mansour Behabadi <mbehabadi@mathspace.com.au>
2023-01-11 14:46:11 +1100

    Install to home/bin dir

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 # Used to download and install lambdafy on a linux/amd64 host
 set -euo pipefail
 _ver="${1:-0.22}"
-cd ~
+mkdir -p ~/bin
+cd ~/bin
 echo "Downloading lambdafy version $_ver ..."
 wget -qO- https://github.com/mathspace/lambdafy/releases/download/v${_ver}/lambdafy_${_ver}_linux_amd64.tar.gz |
 gzip -d |
 tar -x lambdafy
-echo "Installed lambdafy version $_ver in ~/lambdafy"
+echo "Installed lambdafy version $_ver in ~/bin/lambdafy"
