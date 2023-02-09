4312cc68a86ce7e6608b714212cbe12f601c56ca
Mansour Behabadi <mbehabadi@mathspace.com.au>
2023-02-09 17:05:58 +1100

    Bump up setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Used to download and install lambdafy on a linux/amd64 host
 set -euo pipefail
-_ver="${1:-0.39}"
+_ver="${1:-0.40}"
 mkdir -p ~/bin
 cd ~/bin
 echo "Downloading lambdafy version $_ver ..."
