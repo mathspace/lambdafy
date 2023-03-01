49628b0ca5e7627d204cf1329310ad689e3e1a64
Mansour Behabadi <mbehabadi@mathspace.com.au>
2023-03-01 22:08:02 +1100

    Use post for cron

--- proxy/main.go
+++ proxy/main.go
@@ -105,14 +105,16 @@ func handle(ctx context.Context, e map[string]json.RawMessage) (any, error) {
 
 func handleCron(ctx context.Context, cronName string) error {
 	u := fmt.Sprintf("http://%s/_lambdafy/cron?name=%s", appEndpoint, url.QueryEscape(cronName))
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u, nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, u, nil)
 	if err != nil {
 		return fmt.Errorf("error creating HTTP request for cron '%s': %v", cronName, err)
 	}
+	req.Header.Add("Content-Length", "0")
 	resp, err := client.Do(req)
 	if err != nil {
 		return fmt.Errorf("error sending HTTP request for cron '%s': %v", cronName, err)
 	}
+	defer resp.Body.Close()
 	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
 		return fmt.Errorf("error sending HTTP request for cron '%s': %v", cronName, resp.Status)
 	}
