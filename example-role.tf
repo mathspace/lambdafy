b4ebb244dae4aef687f74f475969d65b208e0b47
Mansour Behabadi <mbehabadi@mathspace.com.au>
2023-04-24 12:58:54 +1000

    Fix JSON syntax error

--- example-role.tf
+++ example-role.tf
@@ -36,7 +36,7 @@ resource "aws_iam_user_policy" "lambdafy" {
       "Action": [
         "scheduler:DeleteScheduleGroup",
         "scheduler:CreateScheduleGroup",
-        "scheduler:CreateSchedule",
+        "scheduler:CreateSchedule"
       ],
       "Resource": ["*"]
     },
